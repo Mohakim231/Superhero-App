097fe6ffa1f0b7b8390bc0982002b3deea6548ad
Mohakim231 <mohamedhakim0381@gmail.com>
2023-03-03 10:25:11 +0000

    Adding routes and navbar

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,3 +3,4 @@ export { default as Heroes } from './Heroes';
 export { default as Hero } from './Hero';
 export { default as Best } from './Best';
 export { default as Aquaman } from './Aquaman';
+export { default as NotFound } from './NotFound';
