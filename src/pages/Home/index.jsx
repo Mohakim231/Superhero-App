844b9e30cb147b699f23c31a55bb6a1451cfaad8
Mohakim231 <mohamedhakim0381@gmail.com>
2023-03-03 12:01:42 +0000

    adding home page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
+import './styles.css'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+      <div className='main-content'>
+        <h3>Welcome to the superhero database</h3>
+        <p>Here you'll find information about all the superheros and their power</p>
+      </div>
+      <Link className='main-links' to='/Heroes'><h2 className='main-link-content'>Heroes</h2></Link>
+      <Link className='main-links' to='/Best'><h2 className='main-link-content'>Number 1 Hero</h2></Link>
+    </>
   )
 }
 
--- src/pages/NotFound/index.jsx
+++ src/pages/NotFound/index.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
+import './styles.css'
 
 const NotFound = () => {
   return (
-    <div>NotFound</div>
+    <div className='notfound'>
+      <h1>404: Page not found</h1>
+      <Link to="/">Return Home</Link>
+    </div>
   )
 }
 
